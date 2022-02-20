009c14172b19bb543d02bb027b7528a59c85c7e0
Giancarlo Pernudi Segura <giancarlopernudi@hotmail.com>
2022-02-19 22:23:56 -0700

    checkpoint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const validUrl = () => {
+    // if ()
+  }
   return <Component {...pageProps} />
 }
 
