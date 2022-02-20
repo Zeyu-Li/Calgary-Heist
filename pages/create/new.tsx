72a7c8a353f484296c296f1e7103b112e4a56e31
Zeyu-Li <zeyu.li23@gmail.com>
2022-02-20 09:35:55 -0600

    working create

--- pages/create/new.tsx
+++ pages/create/new.tsx
@@ -1,16 +1,27 @@
-import { useRouter } from 'next/router'
-import { getAuth } from '@firebase/auth'
-import { collection, addDoc, getFirestore } from 'firebase/firestore'
-import { useAuthState } from 'react-firebase-hooks/auth'
+import { useRouter } from "next/router";
+import { getAuth } from "@firebase/auth";
+import { collection, addDoc, getFirestore } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
 
-export default async function Redirect() {
-	const [user, loading] = useAuthState(getAuth());
-	if (loading) return ''
-	const router = useRouter();
-	const db = getFirestore();
-	const docRef = await addDoc(collection(db, 'game'), {
-		creator: user?.uid
-	})
-	router.push(`/create/${docRef.id}`)
-	return 'yeet'
+export default function Redirect() {
+  const router = useRouter();
+  const [user, loading] = useAuthState(getAuth());
+  const db = getFirestore();
+  useEffect(() => {
+    const init = async () => {
+      let docRef: any;
+      try {
+        docRef = await addDoc(collection(db, "game"), {
+          creator: user?.uid,
+        });
+      } catch (err) {
+        alert(`Could not create game ${err}`);
+        if (router) router.push(`/game/`);
+      }
+      if (router) router.push(`/create/${docRef.id}`);
+    };
+    if (!loading) init();
+  }, [loading, router, db]);
+  return <></>;
 }
