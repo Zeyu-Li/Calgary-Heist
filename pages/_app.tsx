e39109a7c49dc8a35fbc43e167d8c164c9405ce6
Zeyu-Li <zeyu.li23@gmail.com>
2022-02-20 09:08:47 -0600

    updated public keys

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,13 +10,13 @@ import Login from "@/components/Login";
 const app = (() => {
   try {
     return initializeApp({
-      apiKey: process.env.NEXT_PUBLIC_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_ID,
-      appId: process.env.NEXT_PUBLIC_APP_ID,
-      measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+      apiKey: "AIzaSyD6kmBY8yHBqmSs8m0IT2QjMxFIv2YP2Tw",
+      authDomain: "calgary-hacks-2022.firebaseapp.com",
+      projectId: "calgary-hacks-2022",
+      storageBucket: "calgary-hacks-2022.appspot.com",
+      messagingSenderId: "475987369364",
+      appId: "1:475987369364:web:406157bea208bf01f04381",
+      measurementId: "G-36BQNL7L59",
     });
   } catch (error) {
     if (error instanceof FirebaseError) return getApp();
